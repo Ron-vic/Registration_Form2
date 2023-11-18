41f90fe742cf47094e4977e1ab301d852d80892a
Ron-vic <137655009+Ron-vic@users.noreply.github.com>
2023-11-18 12:08:10 +0300

    Add files via upload

---
+++ Registration_form2/src/registration_form2/Registration_form2.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package registration_form2;
+
+/**
+ *
+ * @author user
+ */
+public class Registration_form2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
